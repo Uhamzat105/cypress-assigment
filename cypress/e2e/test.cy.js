2b9283d3aecd129c783625ac0e3fbf517fb274ba
Uhamzat105 <uhamzat105@gmail.com>
2025-08-06 02:43:01 +0930

    first commit

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,11 @@
+describe('LOGIN', () => {
+  it('STANDARD USER', () => {
+    cy.visit('https://saucedemo.com/')
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+  })
+})
